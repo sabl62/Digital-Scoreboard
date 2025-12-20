2504499da1584e18f8e30374e3da1eff8e1b15fc
sabal62 <sabalplayz.new@gmail.com>
2025-12-20 21:55:25 +0545

    Empty repository

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('get/', views.get_scoreboard_data, name='get_scoreboard'),
-    path('update/', views.update_scoreboard, name='update_scoreboard'),
-    path('reset/', views.reset_scoreboard, name='reset_scoreboard'),
-    path('tick/', views.increment_time, name='increment_time'),
-]
