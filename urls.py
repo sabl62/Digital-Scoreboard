44908a84a0d03ed5350fcd0946bc2b4e4e43c998
sabal62 <sabalplayz.new@gmail.com>
2025-12-20 21:59:25 +0545

    Digital Scoreboard For Basketball KMC

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('get/', views.get_scoreboard_data, name='get_scoreboard'),
+    path('update/', views.update_scoreboard, name='update_scoreboard'),
+    path('reset/', views.reset_scoreboard, name='reset_scoreboard'),
+    path('tick/', views.increment_time, name='increment_time'),
+]
